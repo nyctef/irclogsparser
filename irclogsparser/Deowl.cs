7e104d6a7f9af6abe6fef6eb23423a87b595e1c2
Nyctef <nyctef@nyctef.com>
2014-04-13 05:15:56 +0100

    track successful deowls

---
+++ irclogsparser/Deowl.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace irclogsparser
+{
+    internal class Deowl : IEquatable<Deowl>
+    {
+        public string Name;
+        public bool Success;
+
+        public Deowl(string name, bool success)
+        {
+            Name = name;
+            Success = success;
+        }
+
+        public bool Equals(Deowl other)
+        {
+            return Name == other.Name && Success == other.Success;
+        }
+
+        public override bool Equals(object other)
+        {
+            return ((var otherDeowl = other as Deowl) != null) && Equals(otherDeowl);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Success.GetHashCode();
+        }
+    }
+}
---
+++ irclogsparser/DeowlTracker.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+namespace irclogsparser
+{
+    internal class DeowlTracker
+    {
+        public DeowlTracker()
+        {
+        }
+
+        internal IEnumerable<Deowl> Run(List<LogMessage> messages)
+        {
+            string lastDeowlAttemptUsername = null;
+            for (int i=0; i<messages.Count;i++)
+            {
+                var message = messages[i];
+                if (message.GetType() == typeof(LogMessage))
+                {
+                    if (message.Message.ToLower().StartsWith("sweetiebot") && 
+                        message.Message.ToLower().Contains("deowl"))
+                    {
+                        lastDeowlAttemptUsername = message.Speaker;
+                    }
+                    else if (message.Speaker.ToLower() == "sweetiebot" &&
+                        message.Message.ToLower().Contains("maybe another time") &&
+                        lastDeowlAttemptUsername != null)
+                    {
+                        yield return new Deowl(lastDeowlAttemptUsername, false);
+                        lastDeowlAttemptUsername = null;
+                    }
+
+                } else if ((var kickedMessage = message as KickedMessage) != null) {
+                    if (i >= 1 && lastDeowlAttemptUsername != null && 
+                        kickedMessage.KickedPerson == ":owl" && 
+                        kickedMessage.Message == ":sweetiestare:")
+                    {
+                        yield return new Deowl(lastDeowlAttemptUsername, true);
+                        lastDeowlAttemptUsername = null;
+                    }
+                }
+            }
+        }
+    }
+}
